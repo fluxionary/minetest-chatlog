1dc24ccba1f757c70436315563043ed4a929d126
y <y@y>
2019-04-25 00:16:17 +0100

    cleanup

--- init.lua
+++ init.lua
@@ -1,20 +1,69 @@
-minetest.log('action', '[chatlog] CSM loading...')
+local mod_name = minetest.get_current_modname()
 
-local register_on_message = core.register_on_sending_chat_message
-if core.register_on_sending_chat_messages then
-	register_on_message = core.register_on_sending_chat_messages
+local function log(level, message)
+    minetest.log(level, ('[%s] %s'):format(mod_name, message))
 end
 
-if register_on_message ~= nil then
-    register_on_message(function(message)
-        local msg = minetest.strip_colors(message)
-        minetest.log('action', '[chatlog:sent] ' .. msg)
-    end)
+log('action', 'CSM loading...')
+
+local server_info = minetest.get_server_info()
+local server_id = server_info.address .. ':' .. server_info.port
+local my_name = ''
+
+local register_on_send = minetest.register_on_sending_chat_message or minetest.register_on_sending_chat_messages
+local register_on_receive = minetest.register_on_receiving_chat_message or minetest.register_on_receiving_chat_messages
+
+
+local function safe(func)
+    -- wrap a function w/ logic to avoid crashing the game
+    local f = function(...)
+        local status, out = pcall(func, ...)
+        if status then
+            return out
+        else
+            log('warning', 'Error (func):  ' .. out)
+            return nil
+        end
+    end
+    return f
 end
 
-if minetest.register_on_receiving_chat_messages ~= nil then
-    minetest.register_on_receiving_chat_messages(function(message)
+
+local set_my_name = safe(function()
+    my_name = minetest.localplayer:get_name()
+    if my_name == nil or my_name == '' then
+        minetest.after(1, set_my_name)
+    end
+end)
+
+
+if minetest.register_on_connect then
+    minetest.register_on_connect(set_my_name)
+elseif minetest.register_on_mods_loaded then
+    minetest.register_on_mods_loaded(set_my_name)
+else
+    set_my_name()
+end
+
+
+if register_on_send then
+    register_on_send(safe(function(message)
         local msg = minetest.strip_colors(message)
-        minetest.log('action', '[chatlog] ' .. msg)
-    end)
+        if msg ~= '' then
+            log('action', ('%s@%s [sent] %s'):format(my_name, server_id, msg))
+        end
+    end))
+else
+    log('warning', 'can\'t find minetest.register_on_sending_chat_message')
+end
+
+if register_on_receive then
+    register_on_receive(safe(function(message)
+        local msg = minetest.strip_colors(message)
+        if msg ~= '' then
+            log('action', ('%s@%s %s'):format(my_name, server_id, msg))
+        end
+    end))
+else
+    log('warning', 'can\'t find minetest.register_on_receiving_chat_message')
 end
